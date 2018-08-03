eaded149a6e1cc0253dd94b671aa829ccd49c4fd
Oliver Schlöbe <github@schloebe.de>
2018-08-03 19:53:11 +0200

    1.1.5
    fluid_styled_content compatibility, closes #2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "jn_lighterbox".
  *
- * Auto generated 23-02-2018 09:07
+ * Auto generated 03-08-2018 19:40
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,12 +21,12 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 1,
-  'version' => '1.1.0',
+  'version' => '1.1.5',
   'constraints' => 
   array (
     'depends' => 
     array (
-      'typo3' => '8.0.0-8.7.99',
+      'typo3' => '8.0.0-9.5.99',
     ),
     'conflicts' => 
     array (
@@ -35,6 +35,6 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  'clearcacheonload' => true
+  'clearcacheonload' => true,
 );
 
