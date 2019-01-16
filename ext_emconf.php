8d2df983aceaa2e4c1f19c7c98312d35c049ef1b
Oliver Schlöbe <github@schloebe.de>
2019-01-16 20:06:36 +0100

    1.1.6
    Maintenance release to fix a composer inconsistency

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 1,
-  'version' => '1.1.5',
+  'version' => '1.1.6',
   'constraints' => 
   array (
     'depends' => 
