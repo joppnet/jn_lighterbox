636a8c1dec161ac203ae1150b00f8465cd1b9f79
Oliver Schlöbe <github@schloebe.de>
2020-06-22 17:13:27 +0200

    1.2.1
    - [FIXED] Lightbox images won't be loaded unprocessed anymore

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,9 @@
 <?php
+
 /***************************************************************
  * Extension Manager/Repository config file for ext "jn_lighterbox".
  *
- * Auto generated 05-11-2019 10:20
+ * Auto generated 05-05-2020 14:49
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -10,23 +11,23 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => '[joppnet] Lightbox',
-	'description' => 'A very lightweight Lightbox extension for click-enlarging content element images using lightbox2 or any other library of your choice.',
-	'category' => 'plugin',
-	'author' => 'Stefan Riedel',
-	'author_email' => 'stefan.riedel@joppnet.de',
-	'author_company' => 'joppnet',
-	'state' => 'stable',
-	'uploadfolder' => false,
-	'createDirs' => '',
-	'clearCacheOnLoad' => 1,
-	'version' => '1.2.0',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '8.7.0-10.9.99',
-		) ,
-		'conflicts' => array() ,
-		'suggests' => array() ,
-	) ,
-	'clearcacheonload' => true,
+  'title' => '[joppnet] Lightbox',
+  'description' => 'A very lightweight Lightbox extension for click-enlarging content element images using lightbox2 or any other library of your choice.',
+  'category' => 'plugin',
+  'author' => 'Stefan Riedel',
+  'author_email' => 'stefan.riedel@joppnet.de',
+  'author_company' => 'joppnet',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 1,
+  'version' => '1.2.1',
+  'constraints' => array(
+    'depends' => array(
+        'typo3' => '8.7.0-10.9.99'
+    ),
+    'conflicts' => array(),
+    'suggests' => array()
+  ),
+  'clearcacheonload' => true
 );
