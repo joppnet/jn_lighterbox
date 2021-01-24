1bfb83724aab7906f60ba31c01d5d69e7be75e62
Oliver Schlöbe <github@schloebe.de>
2021-01-24 19:34:57 +0100

    1.2.4
    - Fixed minor TypoScript Symfony Conditions syntax issue for TYPO3 v9.5+
    - Lightbox updated to latest version

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,14 +3,14 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "jn_lighterbox".
  *
- * Auto generated 05-05-2020 14:49
+ * Auto generated 24-01-2021 18:57
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
   'title' => '[joppnet] Lightbox',
   'description' => 'A very lightweight Lightbox extension for click-enlarging content element images using lightbox2 or any other library of your choice.',
   'category' => 'plugin',
@@ -19,15 +19,22 @@ $EM_CONF[$_EXTKEY] = array(
   'author_company' => 'joppnet',
   'state' => 'stable',
   'uploadfolder' => false,
-  'createDirs' => '',
   'clearCacheOnLoad' => 1,
-  'version' => '1.2.3',
-  'constraints' => array(
-    'depends' => array(
-        'typo3' => '8.7.0-10.9.99'
+  'version' => '1.2.4',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '8.7.0-10.9.99',
     ),
-    'conflicts' => array(),
-    'suggests' => array()
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
   ),
-  'clearcacheonload' => true
+  'clearcacheonload' => true,
+  'createDirs' => NULL,
 );
+
