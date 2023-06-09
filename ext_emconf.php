5dd0c8d881c3457fd2945472cb66e84b44b4d40c
Oliver Schlöbe <github@schloebe.de>
2023-06-09 17:10:54 +0200

    1.2.8
    [FIXED] Update bundled jQuery to latest version
    [FIXED] Code clean-up (#14)

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "jn_lighterbox".
  *
- * Auto generated 29-10-2021 11:58
+ * Auto generated 09-06-2023 16:38
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -11,21 +11,27 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-    'title' => '[joppnet] Lightbox',
-    'description' => 'A very lightweight Lightbox extension for click-enlarging content element images using lightbox2 or any other library of your choice.',
-    'category' => 'plugin',
-    'version' => '1.2.7',
-    'state' => 'stable',
-    'clearCacheOnLoad' => true,
-    'author' => 'Stefan Riedel',
-    'author_email' => 'stefan.riedel@joppnet.de',
-    'author_company' => 'joppnet',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '9.5.0-12.4.99',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(),
+  'title' => '[joppnet] Lightbox',
+  'description' => 'A very lightweight Lightbox extension for click-enlarging content element images using lightbox2 or any other library of your choice.',
+  'category' => 'plugin',
+  'version' => '1.2.8',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'clearcacheonload' => true,
+  'author' => 'Stefan Riedel',
+  'author_email' => 'stefan.riedel@joppnet.de',
+  'author_company' => 'joppnet',
+  'constraints' =>
+  array(
+    'depends' =>
+    array(
+      'typo3' => '9.5.0-12.4.99',
     ),
+    'conflicts' =>
+    array(
+    ),
+    'suggests' =>
+    array(
+    ),
+  ),
 );
